0d39d7f3241778ae1235e1bddabddaec0c15bce0
Jorge Urquijo <jorgedzo_17@hotmail.com>
2018-11-20 10:23:03 -0700

    categories service fix

--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { resolve } from 'path';
 
 @Injectable({
   providedIn: 'root'
